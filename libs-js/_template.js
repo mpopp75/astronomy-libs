6c957de05cc5d353670dd497d52fff19391336b6
Markus Popp <git@mpopp.net>
2013-11-04 03:57:55 +0100

    Add description to JS template function

--- libs-js/_template.js
+++ libs-js/_template.js
@@ -1,3 +1,10 @@
+/*
+ * AstronomyLibs_Template_int2Float(number)
+ *
+ * convert integer to float
+ *
+ * @author Markus Popp <git@mpopp.net>
+ */
 function AstronomyLibs_Template_int2Float(number) {
     var outvar = parseFloat(number.toString());
 
