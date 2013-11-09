0d46d5a32a827de48ed6318f79bd9e18d6bbf334
Markus Popp <git@mpopp.net>
2013-11-09 02:17:19 +0100

    Remove templates

--- examples-php/_template.ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../libs-php/_Template.php';
-use mpopp75\AstronomyLibs as alibs;
-
-if (isset($_POST['dosomething'])) {
-    $in = $_POST['dosomething'];
-
-    print alibs\Template::doSomething($in);
-}
