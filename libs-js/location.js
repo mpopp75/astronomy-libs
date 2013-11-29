89cb919f85e3666c8a43d54ae060e46ec7164573
Markus Popp <git@mpopp.net>
2013-11-29 06:40:51 +0100

    Implement JS: AstronomyLibs_Location_Current
    Function to retrieve current position by
    Geolocation API

--- libs-js/location.js
+++ libs-js/location.js
@@ -21,3 +21,46 @@ function AstronomyLibs_Location(latitude, longitude) {
     return {'latitude':  parseFloat(latitude),
             'longitude': parseFloat(longitude) };
 }
+
+/*
+ * AstronomyLibs_Location_Current()
+ *
+ * return latitude and longitude of current location, using GeoLocation API
+ *
+ * param: callback
+ * author: Markus Popp <git@mpopp.net>
+ * license: http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+function AstronomyLibs_Location_Current(callback) {
+    if (! navigator.geolocation) {
+        return null;
+    }
+
+    var latitude  = null;
+    var longitude = null;
+    var altitude  = null;
+    var accuracy  = null;
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+            latitude  = position.coords.latitude;
+            longitude = position.coords.longitude;
+            altitude  = position.coords.altitude;
+            accuracy  = position.coords.accuracy;
+
+            callback.call( {'latitude':  latitude,
+                            'longitude': longitude,
+                            'alitude':   altitude,
+                            'accuracy':  accuracy }
+            );
+        },
+        function (error) {
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
+
+    return false;
+}
